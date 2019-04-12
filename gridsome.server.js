b90e8164c0960ecd107a23c200ae5204f911c48c
Nigel Bunner <nigelbunner@gmail.com>
2019-04-12 21:42:50 +0100

    Initial commit

---
+++ gridsome.server.js
@@ -0,0 +1,30 @@
+// Server API makes it possible to hook into various parts of Gridsome
+// on server-side and add custom data to the GraphQL data layer.
+// Learn more: https://gridsome.org/docs/server-api
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+const axios = require('axios')
+
+module.exports = function (api) {
+  api.loadSource(async store => {
+    const { data } = await axios.get(process.env.LOCATIONS_API_URL)
+
+    const contentType = store.addContentType({
+      typeName: 'Locations',
+    })
+    console.log(data);
+    for (const item of data) {
+      contentType.addNode({
+        id: item.id,
+        fields: {
+          name: item.name,
+          capital: item.capital,
+          latitude: item.latitude,
+          longitude: item.longitude
+        }        
+      })
+    }
+  })
+}
