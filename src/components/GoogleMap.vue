cff8df2c13e2a455873826c1ad935cd67e788149
Nigel Bunner <nigelbunner@gmail.com>
2019-04-13 22:49:30 +0100

    Setting markers up

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,19 +1,29 @@
 <template>
-    <div>
-        <div v-for="loc in locations">
-            {{ loc.node.name }}<br />
-            {{ loc.node.capital }}<br />
-            {{ loc.node.latitude }}<br />
-            {{ loc.node.longitude }}<br />
-        </div>
+  <div>
+    <div v-for="loc in locations">
+      {{ loc.node.name }}
+      <br>
+      {{ loc.node.capital }}
+      <br>
+      {{ loc.node.latitude }}
+      <br>
+      {{ loc.node.longitude }}
+      <br>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        locations: Array,
-        required: true
+  props: {
+    locations: {
+      type: Array,
+      required: true
+    },
+    markers: {
+      type: Array,
+      required: true
     }
-}
+  }
+};
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,31 +1,44 @@
 <template>
   <Layout>
     <div>
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-    
-    <h1>Hello, world!</h1>
-   
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-    <GoogleMap :locations="$page.allLocations.edges" />
-  </div>
+      <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+      <g-image alt="Example image" src="~/favicon.png" width="135"/>
 
+      <h1>Hello, world!</h1>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores</p>
+      <GoogleMap :locations="$page.allLocations.edges" :markers="getMarkers()"/>
+    </div>
   </Layout>
 </template>
 
 <script>
-import GoogleMap from '~/components/GoogleMap.vue'
+import GoogleMap from "~/components/GoogleMap.vue";
 
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: "Hello, world!"
   },
   components: {
     GoogleMap
+  },
+  methods: {
+    getMarkers() {
+      let markers = [];
+      for (let i = 0; i < this.$page.allLocations.edges.length; i++) {
+        markers = markers.concat({
+          position: {
+            lat: parseFloat(this.$page.allLocations.edges[i].node["latitude"]),
+            lng: parseFloat(this.$page.allLocations.edges[i].node["longitude"])
+          },
+          clickable: true,
+          draggable: false
+        });
+      }
+      return markers;
+    }
   }
-}
+};
 </script>
 
 <style>
