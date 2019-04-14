7c58e5c1f12f9940d7ab0786c8f24b1144999fd2
Nigel Bunner <nigelbunner@gmail.com>
2019-04-14 20:57:20 +0100

    Adding Googlemaps plugin

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
+    <GmapMap
+      :center="{lat:10, lng:10}"
+      :zoom="7"
+      map-type-id="terrain"
+      style="width: 500px; height: 300px"
+    >
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="center=m.position"
+      />
+    </GmapMap>
     <div v-for="loc in locations">
       {{ loc.node.name }}
       <br>
@@ -14,7 +29,11 @@
 </template>
 
 <script>
+// import GmapMap from "vue2-google-maps";
+// import GmapMarker from "vue2-google-maps";
+
 export default {
+  mounted() {},
   props: {
     locations: {
       type: Array,
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
+import DefaultLayout from "~/layouts/Default.vue";
+import * as VueGoogleMaps from "vue2-google-maps";
 
-export default function (Vue, { router, head, isClient }) {
+export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
+  Vue.component("Layout", DefaultLayout);
+  Vue.use(VueGoogleMaps, {
+    load: {
+      key: process.env.GRIDSOME_GOOGLEMAPS_API_KEY
+    }
+  });
 }
